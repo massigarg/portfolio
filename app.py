f25cd0d43b376bda1c6ff9359c710b94ded398b6
Massimiliano <mas.gargano@gmail.com>
2022-02-02 21:44:36 +0100

    added TDD article

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template, url_for, request, jsonify
-# from flask_wtf import FlaskForm
-# from wtforms import BooleanField
 
 
 app = Flask(__name__)
 app.secret_key = ""
-
-text = 'The Biden administration plans to require most foreign visitors to be vaccinated.Biden Plans New Policy Requiring That All Foreign Travelers to U.S. Be VaccinatedThe Biden administration is developing plans to require all foreign travelers to the United States to be vaccinated against Covid-19, with limited exceptions, according to an administration official with knowledge of the developing policy.Officials say the new policy is being readied in the event that the United States eases its travel rules, which isn’t expected soon.'
 
 
 @app.route("/")
@@ -19,8 +15,6 @@ def home():
 def about():
     return render_template("about.html", title="About")
 
-# keyword_extractor(text, top_n, diversity, mmr)
-
 
 @app.route('/extract_keywords', methods=['POST', 'GET'])
 def extract_keywords():
@@ -28,13 +22,19 @@ def extract_keywords():
     mmr = request.args.get('mmr', type=bool)
     diversity = request.args.get('diversity', 0.2, type=float)
     top_n = request.args.get('top_n', 6, type=int)
+    text = 'The Biden administration plans to require most foreign visitors to be vaccinated.Biden Plans New Policy Requiring That All Foreign Travelers to U.S. Be VaccinatedThe Biden administration is developing plans to require all foreign travelers to the United States to be vaccinated against Covid-19, with limited exceptions, according to an administration official with knowledge of the developing policy.Officials say the new policy is being readied in the event that the United States eases its travel rules, which isn’t expected soon.'
+
     return jsonify(result=keyword_extractor(text, top_n, diversity, mmr), )
 
 
-@app.route("/first_article", methods=["POST", "GET"])
-def first_article():
+@app.route("/keywrod_assignment_with_keybert", methods=["POST", "GET"])
+def keywrod_assignment_with_keybert():
+    return render_template("keywrod_assignment_with_keybert.html", title="Article")
 
-    return render_template("first_article.html", title="Article")
+
+@app.route("/TDD")
+def TDD():
+    return render_template("TDD.html", title="Article")
 
 
 if __name__ == "__main__":
